c9958e8b213bb5af5877d18dc5b128981b413753
sevixoo <sewerun777@gmail.com>
2016-05-24 18:10:45 +0200

    CCC
    sdasd

--- src/AppBundle/Controller/FormController.php
+++ src/AppBundle/Controller/FormController.php
@@ -71,6 +71,21 @@ class FormController extends Controller
 
         $conn->exec($sql);
 
+        $form_id = $conn->lastInsertId();
+
+        $sql = "SELECT DISTINCT Subscribers.id , Subscribers.email FROM `GroupsSubscribers`
+                JOIN Subscribers ON Subscribers.id = idSubscriber
+                WHERE `idGroup` IN (181,170)";
+        $users= $conn->fetchAll($sql);
+
+        foreach($users as $u){
+            $UUID = $this->gen_uuid();
+            $id = $u['id'];
+            $sql = "INSERT INTO `FormOutputs`(`id`, `subscriber_id`, `form_id`, `token`, `output`, `pre_output`)
+                    VALUES (null,$id,$form_id,\"$UUID\",null,null)";
+            $conn->exec($sql);
+        }
+
         return $this->redirectToRoute('forms');
     }
 
@@ -98,5 +113,25 @@ class FormController extends Controller
 
         return $this->redirectToRoute('forms');
     }
+
+
+    function gen_uuid() {
+        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+            // 16 bits for "time_mid"
+            mt_rand( 0, 0xffff ),
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand( 0, 0x0fff ) | 0x4000,
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand( 0, 0x3fff ) | 0x8000,
+            // 48 bits for "node"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+    }
+
 
 }
